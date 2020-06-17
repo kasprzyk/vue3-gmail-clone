8f5acd06995a4ed42670138c2a52b9ee36e1987e
kasprzyk <piotkas1@gmail.com>
2020-06-17 13:43:10 +0200

    MailTable component

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,22 @@
 <template>
   <h1>VMail Inbox</h1>
-  <table class="mail-table">
-    <tbody>
-      <tr
-        v-for="email in unarchivedEmails"
-        :key="email.id"
-        :class="['clickable', email.read ? 'read' : '']"
-        @click="email.read = true"
-      >
-        <td>
-          <input type="checkbox" />
-        </td>
-        <td>{{ email.from }}</td>
-        <td>
-          <p>
-            <strong>{{ email.subject }}</strong> - {{ email.body }}
-          </p>
-        </td>
-        <td class="date">{{ format(new Date(email.sentAt), "MMM do yyyy") }}</td>
-        <td>
-          <button @click="email.archived = true">Archive</button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <suspense>
+    <template #default>
+      <MailTable></MailTable>
+    </template>
+    <template #fallback>
+      <h1>Mails are loading, please wait</h1>
+    </template>
+  </suspense>
 </template>
 
 <script>
 import { format } from "date-fns";
+import MailTable from "@/components/MailTable.vue";
 export default {
   name: "App",
-  data() {
-    return {
-      format,
-      emails: [
-        {
-          id: 1,
-          from: "piotkas1@gmail.com",
-          subject: "test",
-          body: "test",
-          sentAt: "2020-03-27T18:25:43.511Z",
-          archived: false,
-          read: true
-        },
-        {
-          id: 2,
-          from: "piotkas1@gmail.com",
-          subject: "test",
-          body: "test",
-          sentAt: "2020-03-27T18:25:43.511Z",
-          archived: false,
-          read: false
-        },
-        {
-          id: 3,
-          from: "piotkas1@gmail.com",
-          subject: "test",
-          body: "test",
-          sentAt: "2020-03-27T18:25:43.511Z",
-          archived: false,
-          read: false
-        },
-        {
-          id: 4,
-          from: "piotkas1@gmail.com",
-          subject: "test",
-          body: "test test test test test",
-          sentAt: "2020-03-27T18:25:43.511Z",
-          archived: true,
-          read: false
-        }
-      ]
-    };
-  },
-  computed: {
-    sortedEmails() {
-      return this.emails.sort((e1, e2) => {
-        return e1.sentAt < e2.sentAt ? 1 : -1;
-      });
-    },
-    unarchivedEmails() {
-      return this.emails.filter(e => !e.archived);
-    }
+  components: {
+    MailTable
   }
 };
 </script>
---
+++ src/components/MailTable.vue
@@ -0,0 +1,49 @@
+<template>
+  <table class="mail-table">
+    <tbody>
+      <tr
+        v-for="email in unarchivedEmails"
+        :key="email.id"
+        :class="['clickable', email.read ? 'read' : '']"
+        @click="email.read = true"
+      >
+        <td>
+          <input type="checkbox" />
+        </td>
+        <td>{{ email.from }}</td>
+        <td>
+          <p>
+            <strong>{{ email.subject }}</strong> - {{ email.body }}
+          </p>
+        </td>
+        <td class="date">{{ format(new Date(email.sentAt), "MMM do yyyy") }}</td>
+        <td><button @click="email.archived = true">Archive</button></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import { format } from "date-fns";
+
+export default {
+  async setup() {
+    return {
+      format,
+      data: [
+        /* ... */
+      ]
+    };
+  },
+  computed: {
+    sortedEmails() {
+      return this.emails.sort((e1, e2) => {
+        return e1.sentAt < e2.sentAt ? 1 : -1;
+      });
+    },
+    unarchivedEmails() {
+      return this.sortedEmails.filter(e => !e.archived);
+    }
+  }
+};
+</script>
